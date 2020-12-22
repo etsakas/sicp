81b1a4741c12abea27450fa1d9254d9951312dcc
stathis94 <zephyroswest@gmail.com>
2020-12-22 20:52:41 +0200

    2.15: unfinished

---
+++ ch2/2.15.scm
@@ -0,0 +1,5 @@
+#lang sicp
+
+; looking at the test results of 2.14 it seems true that par2 produces tighter error bounds 
+; even though the formulas are algebraically equivalent.
+; I can't properly prove it, but intuitively, the more we use uncertain numbers, the more the final uncertainty
